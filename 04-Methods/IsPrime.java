6fbcb78e09a4cee2dcb96e67d2d2785818f85f09
Jitesh Yadav <jituu098@gmail.com>
2026-07-30 19:49:30 +0530

    arryas basic with example added

---
+++ 04-Methods/IsPrime.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class IsPrime{
+    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        isPrime(78);
+    }
+
+     static void isPrime(int num) {
+        System.out.println("Enter number to check isPrime or not: ");
+        // int num = input.nextInt();
+        }
+    
+    
+}
